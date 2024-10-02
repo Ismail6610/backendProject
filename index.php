b54ece90bf7d93614f8ef61d51ebfb5affdf6372
Ismail Bajrovic <115583520+Ismail6610@users.noreply.github.com>
2024-10-02 14:29:55 +0200

    Printing out products

--- index.php
+++ index.php
@@ -9,6 +9,27 @@ include 'config.php'; ?>
     <title>Document</title>
 </head>
 <body>
-    ROARGHHHHHHHHHHHHHHHHHHHHHHHHH
+    <header>header</header>
+    <main>main
+        <?php 
+        $sql = "SELECT id, product, description, price, image FROM products";
+        $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<div>";
+                echo "<p>ID: " . $row['id'] . "</p>";
+                echo "<p>Product: " . $row['product'] . "</p>";
+                echo "<p>Description: " . $row['description'] . "</p>";
+                echo "<p>Price: $" . $row['price'] . "</p>";
+                echo "<p><img src='" . $row['image'] . "' alt='" . $row['product'] . "'></p>";
+                echo "</div>";
+            }
+        }else{
+            echo "<p>No products found.</p>";
+        }
+        $conn->close();
+        ?>
+    </main>
+    <footer>footer</footer>
 </body>
 </html>
